9a1ee1c9a1bd703c2245413b837f5a4aa8bc5f66
jer3m01 <jer3m01@jer3m01.com>
2020-05-21 00:58:57 +0200

    Added account OAuth linking & unlinking routes

--- routes/base.php
+++ routes/base.php
@@ -3,6 +3,9 @@
 Route::get('/', 'IndexController@index')->name('index')->fallback();
 Route::get('/account', 'IndexController@index')->name('account');
 
+Route::get('/account/oauth/link', 'OAuthController@link')->name('account.oauth.link');
+Route::get('/account/oauth/unlink', 'OAuthController@unlink')->name('account.oauth.unlink');
+
 Route::get('/locales/{locale}/{namespace}.json', 'LocaleController')
     ->where('namespace', '.*');
 
